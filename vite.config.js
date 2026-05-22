95c01b2eed7e4e3b445d1f2fcd491a4b40b94efc
mayur-3112 <mayur311agarwal@gmail.com>
2026-05-22 08:58:23 +0530

    chore: configure github pages deployment pipeline

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
 
 export default defineConfig({
+  base: './',
   plugins: [react(), glsl()],
   build: {
     target: "esnext",
